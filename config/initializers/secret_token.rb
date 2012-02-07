0263d6c122f67defc8b4bafd58bf75dac936d4b8
Diego Gomes <diegorg95@hotmail.com>
2012-02-07 10:24:02 -0200

    criação do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enciclofrutas::Application.config.secret_token = 'c7c85b0aa6f78691817143671ca947390ee6a04041aae3d4dd7eb7973d08bc321e00e6630da6c12f87f79738b1904bdb4d23b8331f9fae1336a420361b35b051'
