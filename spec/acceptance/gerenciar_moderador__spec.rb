47b3463a9251c6ac8ea0a831c42d2f5e3b05478a
Diego Gomes <diegorg95@hotmail.com>
2012-02-14 12:11:41 -0200

    Genreciar Enciclofrutas_alterar

--- spec/acceptance/gerenciar_moderador__spec.rb
+++ spec/acceptance/gerenciar_moderador__spec.rb
@@ -34,6 +34,40 @@ feature 'gerenciar moderador' do
     page.should have_content 'e-mail: diego@email.com'
   end
 
+
+  scenario 'alterar moderador' do #, :javascript => true do
+    moderador = FactoryGirl.create(:moderador)
+    visit edit_moderador_path(moderador)
+        fill_in 'Nome', :with => 'Diego'
+        fill_in 'Sobrenome', :with => 'Gomes'
+        fill_in 'Senha', :with => '123456'
+        fill_in 'Pais', :with => 'Brasil'
+        fill_in 'Cidade', :with => 'vitória'
+        fill_in 'Genero', :with => 'macho'
+        fill_in 'Data_Nasc', :with => '04/02/1990'
+        fill_in 'CPF', :with => '123456789-10'
+        fill_in 'RG', :with => '256.363.256-45'
+        fill_in 'e-mail', :with => 'diego@email.com'
+   
+        
+    click_button 'Salvar'
+        
+        page.should have_content 'Nome: Diego'
+        page.should have_content 'Sobrenome: Gomes'
+        page.should have_content 'Senha: 123456'
+        page.should have_content 'Pais: Brasil'
+        page.should have_content 'Cidade: vitória'
+        page.should have_content 'Genero: macho'
+        page.should have_content 'Data_Nasc: 04/02/1990'
+        page.should have_content 'CPF: 123456789-10'
+        page.should have_content 'RG: 256.363.256-45'
+        page.should have_content 'e-mail: diego@email.com'
+       
+    
+    
+  end
+
+  
  
 end
 
---
+++ spec/factories/moderadors.rb
@@ -0,0 +1,16 @@
+# Read about factories at http://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :moderador do
+    nome "MyString"
+    sobrenome "MyString"
+    senha "MyString"
+    pais "MyString"
+    cidade "MyString"
+    genero "MyString"
+    data_nasc "MyString"
+    cpf "MyString"
+    rg "MyString"
+    
+  end
+end
