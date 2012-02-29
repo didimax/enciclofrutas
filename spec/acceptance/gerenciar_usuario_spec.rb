2525430a651431672b9220b4c26977f03fe96c5b
Diego Gomes <diegorg95@hotmail.com>
2012-02-29 11:07:43 -0300

    Gerenciar Enciclofrutas_usuario_excluir

--- spec/acceptance/gerenciar_usuario_spec.rb
+++ spec/acceptance/gerenciar_usuario_spec.rb
@@ -25,4 +25,39 @@ feature 'gerenciar usuario' do
     page.should have_content 'Senha: 12345'
     page.should have_content 'email: diego@email.com'
   end
+
+    scenario 'alterar usuario' do #, :javascript => true do
+    usuario = FactoryGirl.create(:usuario)
+    visit edit_usuario_path(usuario)
+
+    fill_in 'Nome', :with => 'Diego'
+    fill_in 'Sobrenome', :with => 'Gomes'
+    fill_in 'Data_Nasc', :with => '04/02/1990'
+    fill_in 'Genero', :with => 'macho'
+    fill_in 'Senha', :with => '12345'
+    fill_in 'email', :with => 'diego@email.com'
+        
+    click_button 'Salvar'
+   
+    page.should have_content 'Nome: Diego'
+    page.should have_content 'Sobrenome: Gomes'
+    page.should have_content 'Data_Nasc: 04/02/1990'
+    page.should have_content 'Genero: macho'
+    page.should have_content 'Senha: 12345'
+    page.should have_content 'email: diego@email.com'
+       
+    
+    
+  end
+
+    scenario 'excluir usuario' do #, :javascript => true do
+    moderador = FactoryGirl.create(:usuario)
+    visit usuarios_path
+
+    click_link 'Excluir'
+    
+    Moderador.count.should == 0
+  end
+ 
+
 end
