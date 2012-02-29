aa8a3b9cfd2f8d3bf1f82123e1e2a0ed80251587
Diego Gomes <diegorg95@hotmail.com>
2012-02-29 12:53:05 -0300

    Gerenciar Enciclofrutas_mercado_alterar_excluir

--- spec/acceptance/gerenciar_mercado_spec.rb
+++ spec/acceptance/gerenciar_mercado_spec.rb
@@ -21,4 +21,28 @@ feature 'gerenciar mercado' do
     
   end 
 
+    scenario 'alterar mercado' do #, :javascript => true do
+    mercado = FactoryGirl.create(:mercado)
+    visit edit_mercado_path(mercado)
+    
+    fill_in 'Nome', :with => 'Wal-Mart'
+    fill_in 'Endereço', :with => 'Parque São Caetano'
+    fill_in 'Telefone', :with => '2788-8989'
+        
+    click_button 'Salvar'
+   
+    page.should have_content 'Nome: Wal-Mart'
+    page.should have_content 'Endereço: Parque São Caetano'
+    page.should have_content 'Telefone: 2788-8989'
+
+end
+    scenario 'excluir vitamina' do #, :javascript => true do
+    mercado = FactoryGirl.create(:mercado)
+    visit mercados_path
+
+    click_link 'Excluir'
+    
+    Mercado.count.should == 0
+  end
+
 end
