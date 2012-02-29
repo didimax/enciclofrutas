68f522e182a04e8389aee56b6af5938f65ad65ca
Diego Gomes <diegorg95@hotmail.com>
2012-02-29 10:12:09 -0300

    Gerenciar Enciclofrutas_fruta_alterar

--- spec/acceptance/gerenciar_fruta_spec.rb
+++ spec/acceptance/gerenciar_fruta_spec.rb
@@ -38,5 +38,38 @@ feature 'gerenciar fruta' do
 
   end
 
+
+    scenario 'alterar fruta' do #, :javascript => true do
+
+    moderador = FactoryGirl.create(:moderador,:nome => 'Diego') 
+    fruta = FactoryGirl.create(:fruta, :moderador => moderador)
+
+    visit edit_fruta_path(fruta)
+
+
+    fill_in 'Nome', :with => 'morango'
+    fill_in 'Nome_Cientifico', :with => 'moraguns'
+    fill_in 'Peso' , :with => '10g'
+    fill_in 'Cor', :with => 'vermelha com pintas pretas'
+    fill_in 'Preço', :with => 'R$5 por kg'
+    fill_in 'Localização', :with => 'Região Sul'
+    fill_in 'Calorias', :with => '45'
+    fill_in 'Informação', :with => 'Morango é a fruta mais cara na Asia'    
+    select 'Diego', :on => 'Nome'
+       
+    click_button 'Salvar'
+  
+  
+    page.should have_content 'Nome: morango'
+    page.should have_content 'Nome_Cientifico: moraguns'
+    page.should have_content 'Peso: 10g'
+    page.should have_content 'Cor: vermelha com pintas pretas'
+    page.should have_content 'Preço: R$5 por kg'
+    page.should have_content 'Localização: Região Sul'
+    page.should have_content 'Calorias: 45'
+    page.should have_content 'Informação: Morango é a fruta mais cara na Asia'
+    page.should have_content 'Nome: Diego'
+
+end
  
 end
